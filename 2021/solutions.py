560f64194d2b1707bd7ca33fab7c3061dc74fe98
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-12-01 11:14:36 +0100

    Solution 2021/1

--- 2021/solutions.py
+++ 2021/solutions.py
@@ -7,6 +7,31 @@ INPUT_FILES = dict((f"day{i+1}", f"2021/inputs/input{i+1}.txt") for i in range(2
 
 # https://adventofcode.com/2021/day/1
 def day1(input_file: str) -> None:
+    """Answer 1 is 1226
+    Answer 2 is 1252
+    """
+    lines = [int(i) for i in input_file.split("\n") if i]
+    increases = 0
+    for index, line in enumerate(lines[:-1]):
+        if lines[index + 1] > line:
+            increases += 1
+    print("Number of depth increases is:", increases)
+
+    increases_window = 0
+    for index, line in enumerate(lines[:-3]):
+        if (
+            line + lines[index + 1] + lines[index + 2]
+            < lines[index + 1] + lines[index + 2] + lines[index + 3]
+        ):
+            increases_window += 1
+    print("Number of 3-measure windo increases is:", increases_window)
+
+
+# https://adventofcode.com/2021/day/2
+def day2(input_file: str) -> None:
+    """Answer 1 is ...
+    Anser 2 is ...
+    """
     pass
 
 
@@ -23,7 +48,7 @@ def solver(day: str) -> None:
 def all_days() -> None:
     """Run all days at once"""
     totaltime = time.time()
-    for i in range(1):
+    for i in range(2):
         print(f"===== DAY {i+1:2d} =====")
         solver(f"day{i+1}")
         print()
@@ -33,5 +58,5 @@ def all_days() -> None:
 
 
 if __name__ == "__main__":
-    solver("day1")
+    solver("day2")
     # all_days()

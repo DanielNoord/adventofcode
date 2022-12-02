e69f91996405ccdea16e0b2ab787c3223dba33bd
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-12-02 21:57:16 +0100

    Improve day2

--- solutions/year2022/day2.py
+++ solutions/year2022/day2.py
@@ -3,28 +3,32 @@ from __future__ import annotations
 # pylint: disable=consider-using-namedtuple-or-dataclass
 
 OUTCOMES = {
-    "A": {"X": 4, "Y": 8, "Z": 3},
-    "B": {"X": 1, "Y": 5, "Z": 9},
-    "C": {"X": 7, "Y": 2, "Z": 6},
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
 }
 NEEDED_TO_GET = {
-    "A": {"X": 3, "Y": 4, "Z": 8},
-    "B": {"X": 1, "Y": 5, "Z": 9},
-    "C": {"X": 2, "Y": 6, "Z": 7},
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
 }
 
 
 def part1(data: str) -> str | int:
-    total = 0
-    for game in data.split("\n"):
-        opp, you = game.split(" ")
-        total += OUTCOMES[opp][you]
-    return total
+    return sum(OUTCOMES[game] for game in data.splitlines())
 
 
 def part2(data: str) -> str | int:
-    total = 0
-    for game in data.split("\n"):
-        opp, you = game.split(" ")
-        total += NEEDED_TO_GET[opp][you]
-    return total
+    return sum(NEEDED_TO_GET[game] for game in data.splitlines())

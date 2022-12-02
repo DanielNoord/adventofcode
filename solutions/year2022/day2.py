94100910ac2678aec99d336391308038692cbaa2
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-12-02 09:12:59 +0100

    2022 day 2 done

--- solutions/year2022/day2.py
+++ solutions/year2022/day2.py
@@ -1,9 +1,28 @@
 from __future__ import annotations
 
+# pylint: disable=consider-using-namedtuple-or-dataclass
+
+OUTCOMES = {
+    "A": {"X": 4, "Y": 8, "Z": 3},
+    "B": {"X": 1, "Y": 5, "Z": 9},
+    "C": {"X": 7, "Y": 2, "Z": 6},
+}
+NEEDED_TO_GET = {
+    "A": {"X": 3, "Y": 4, "Z": 8},
+    "B": {"X": 1, "Y": 5, "Z": 9},
+    "C": {"X": 2, "Y": 6, "Z": 7},
+}
+
 
 def part1(data: str) -> str | int:
-    raise NotImplementedError("Part 1 not implemented for 2022 day 2")
+    return sum(
+        OUTCOMES[opp][you]
+        for opp, you in (game.split(" ") for game in data.split("\n"))
+    )
 
 
 def part2(data: str) -> str | int:
-    raise NotImplementedError("Part 2 not implemented for 2022 day 2")
+    return sum(
+        NEEDED_TO_GET[opp][you]
+        for opp, you in (game.split(" ") for game in data.split("\n"))
+    )

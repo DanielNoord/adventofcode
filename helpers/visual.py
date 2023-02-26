5659e0ab19bc4e07b5aeebda3641106b806f9813
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-02-26 20:43:06 +0100

    Improve coords printer

--- helpers/visual.py
+++ helpers/visual.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 
-def pretty_print_list_of_coords(coords: list[tuple[int, int]]) -> None:
+
+def pretty_print_list_of_coords(
+    coords: Iterable[tuple[int, int]], reverse: bool = False
+) -> None:
     """Transform a list of coordinates into a grid with filled in squares."""
     max_x = max(c[0] for c in coords)
     min_x = min(c[0] for c in coords)
     max_y = max(c[1] for c in coords)
     min_y = min(c[1] for c in coords)
 
+    strings: list[str] = []
     for y_coord in range(max_y, min_y - 1, -1):
         string = [
             "#" if (x, y_coord) in coords else "." for x in range(min_x, max_x + 1)
         ]
-        print("".join(string))
+        strings.append("".join(string))
+
+    if reverse:
+        strings.reverse()
+
+    print("\n".join(strings))

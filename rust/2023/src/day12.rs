bc6e970ce72868a88c590b47d813425c46601d3d
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-12-15 12:44:18 +0100

    WIP part 2 for day 12

--- rust/2023/src/day12.rs
+++ rust/2023/src/day12.rs
@@ -1,104 +1,122 @@
-use std::{collections::HashSet, iter::zip};
+use std::collections::HashMap;
 
-use itertools::Itertools;
+#[derive(Debug, Clone, PartialEq)]
+enum Sign {
+    Broken,
+    Fixed,
+    Question,
+}
 
-fn partition_numbers(numbers: u32, bins: u32, lowest: u32) -> HashSet<Vec<u32>> {
-    let mut final_bins: HashSet<Vec<u32>> = HashSet::new();
-
-    match bins {
-        1 => {
-            final_bins.insert(vec![numbers]);
+impl From<char> for Sign {
+    fn from(value: char) -> Self {
+        match value {
+            '?' => Sign::Question,
+            '#' => Sign::Broken,
+            '.' => Sign::Fixed,
+            _ => panic!(),
         }
-        _ if bins > 1 => {
-            for i in lowest..(numbers / 2 + 1) {
-                for p in partition_numbers(numbers - i, bins - 1, i) {
-                    let mut bin = p;
-                    bin.push(i);
-                    final_bins.insert(bin);
-                }
-            }
+    }
+}
+
+fn find_matching_arrangement(
+    previous_sign: &Sign,
+    diagram: Vec<Sign>,
+    spacing: Vec<u32>,
+) -> u32 {
+    if diagram.is_empty() {
+        if spacing.is_empty() {
+            return 1;
+        } else {
+            return 0;
         }
-        _ => (),
     }
 
-    final_bins
+    match diagram[0] {
+        Sign::Question => {
+            let mut intermediate_total = 0;
+
+            let mut new_diagram = diagram.clone();
+            new_diagram[0] = Sign::Broken;
+            println!("{:?}", diagram);
+            println!("{:?}", new_diagram);
+            intermediate_total +=
+                find_matching_arrangement(previous_sign, new_diagram, spacing.clone());
+
+            let mut new_diagram = diagram.clone();
+            new_diagram[0] = Sign::Fixed;
+            intermediate_total +=
+                find_matching_arrangement(previous_sign, new_diagram, spacing.clone());
+            intermediate_total
+        }
+        Sign::Broken => {
+            if spacing.is_empty() {
+                return 0;
+            }
+
+            match previous_sign {
+                Sign::Broken => 0,
+                Sign::Fixed => {
+                    let broken_length: usize = spacing[0] as usize;
+                    if diagram.len() < broken_length {
+                        return 0;
+                    }
+                    if diagram[0..broken_length].iter().any(|e| e == &Sign::Fixed) {
+                        return 0;
+                    }
+
+                    let spacing = spacing[1..].to_vec();
+                    let diagram = diagram[broken_length..].to_vec();
+                    find_matching_arrangement(&Sign::Broken, diagram, spacing)
+                }
+                _ => panic!(),
+            }
+        }
+        Sign::Fixed => {
+            let diagram = diagram[1..].to_vec();
+
+            match previous_sign {
+                Sign::Broken => {
+                    find_matching_arrangement(&Sign::Fixed, diagram, spacing)
+                }
+                Sign::Fixed => {
+                    find_matching_arrangement(&Sign::Fixed, diagram, spacing)
+                }
+                _ => panic!(),
+            }
+        }
+    }
 }
 
 pub fn part1(input: &str) -> String {
     let mut total = 0;
+
     for line in input.lines() {
-        let mut split_line = line.split(' ');
-        let current_diagram = split_line.next().unwrap();
-        let spacing = split_line.next().unwrap();
-
-        let diagram_length = current_diagram.len();
-        let mut dividers: u32 = 2;
-        let mut space_for_dividers: u32 = diagram_length as u32;
-        let mut space_indicators: Vec<u32> = Vec::new();
-
-        for instruction in spacing.replace(',', " , ").split(' ') {
-            if instruction == "," {
-                dividers += 1;
-                space_for_dividers -= 1;
-            } else {
-                let value: u32 = instruction.parse().unwrap();
-                space_for_dividers -= value;
-                space_indicators.push(value);
-            }
-        }
-
-        let partitions: HashSet<Vec<u32>> = HashSet::from_iter(
-            partition_numbers(space_for_dividers, dividers, 0)
-                .into_iter()
-                .flat_map(|e| e.clone().into_iter().permutations(e.len())),
+        let (diagram, spacing) = line.split_once(' ').unwrap();
+        total += find_matching_arrangement(
+            &Sign::Fixed,
+            Vec::from_iter(diagram.chars().map(|e| e.try_into().unwrap())),
+            Vec::from_iter(spacing.split(',').map(|e| e.parse().unwrap())),
         );
-
-        for mut final_partition in partitions {
-            let len = &final_partition.len();
-            for (index, value) in final_partition.iter_mut().enumerate() {
-                if index != 0 && &index != len {
-                    *value += 1;
-                }
-            }
-            let mut damaged_indicators = space_indicators.clone();
-            damaged_indicators.push(0);
-            let mut indicator_iter = damaged_indicators.iter();
-            let mut diagram = String::new();
-            for divider_count in final_partition {
-                diagram += &".".repeat(divider_count as usize);
-                diagram += &"#".repeat(*indicator_iter.next().unwrap() as usize);
-            }
-
-            if space_indicators.len()
-                != diagram.replace('.', " ").split_ascii_whitespace().count()
-            {
-                continue;
-            }
-
-            let mut good = true;
-            for (c, d) in zip(current_diagram.chars(), diagram.chars()) {
-                match c {
-                    '?' => (),
-                    _ => {
-                        if c != d {
-                            good = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            if good {
-                total += 1;
-            }
-        }
     }
 
-    assert!(total == 8193, "Should be 8193");
+    assert!(total == 8193, "Should be 8193 {total}");
     total.to_string()
 }
 
-pub fn part2(_input: &str) -> String {
-    let total = 0;
+pub fn part2(input: &str) -> String {
+    let mut total = 0;
+
+    for (index, line) in input.lines().enumerate() {
+        println!("{}", index);
+        let (diagram, spacing) = line.split_once(' ').unwrap();
+        let diagram = [diagram; 5].join("?");
+        let spacing = [spacing; 5].join(",");
+        total += find_matching_arrangement(
+            &Sign::Fixed,
+            Vec::from_iter(diagram.chars().map(|e| e.try_into().unwrap())),
+            Vec::from_iter(spacing.split(',').map(|e| e.parse().unwrap())),
+        );
+    }
 
     // Should be 45322533163795
     total.to_string()
